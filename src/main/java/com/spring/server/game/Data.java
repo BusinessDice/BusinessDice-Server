b53cc1e5339e5fec42849606e78e02a35a6c0c33
Felix Steinke <82stfe1bif@hft-stuttgart.de>
2021-01-03 18:44:37 +0100

    updated encoding

--- src/main/java/com/spring/server/game/Data.java
+++ src/main/java/com/spring/server/game/Data.java
@@ -18,44 +18,44 @@ public class Data {
 
     public Data() {
         projectCards.put(BAHNHOF,
-                new ProjectCard(ProjectCardEntity.BAHNHOF, UNTERNEHMEN, 4, "Der Spieler darf in jedem Zug neu entscheiden, ob er 1 oder 2 Würfel verwendet."));
+                new ProjectCard(ProjectCardEntity.BAHNHOF, UNTERNEHMEN, 4, "Der Spieler darf in jedem Zug neu entscheiden, ob er 1 oder 2 Wuerfel verwendet."));
         projectCards.put(EINKAUFSZENTRUM,
-                new ProjectCard(EINKAUFSZENTRUM, UNTERNEHMEN, 10, "Immer wenn der Spieler für ein Unternehmen mit einem der abgebildeten Symbole Einkommen erhält, bekommt er für jedes dieser gewürfelten Unternehmen 1 Münze mehr. Hat er z.B. 2 Bäckereien, erhält er 4 statt 2 Münzen. Hat er z.B. 1 Familien-Restaurant, erhält er 3 statt 2 Münzen."));
+                new ProjectCard(EINKAUFSZENTRUM, UNTERNEHMEN, 10, "Immer wenn der Spieler fuer ein Unternehmen mit einem der abgebildeten Symbole Einkommen erhaelt, bekommt er fuer jedes dieser gewuerfelten Unternehmen 1 Muenze mehr. Hat er z.B. 2 Baeckereien, erhaelt er 4 statt 2 Muenzen. Hat er z.B. 1 Familien-Restaurant, erhaelt er 3 statt 2 Muenzen."));
         projectCards.put(FREIZEITPARK,
-                new ProjectCard(FREIZEITPARK, UNTERNEHMEN, 16, "Nur wenn der Spieler seinen Bahnhof gebaut hat, darf er auch mit 2 Würfeln würfeln. Er kann dadurch 2 gleiche Zahlen erhalten (Pasch). Der Spieler hat in diesem Fall einen vollständigen zweiten Spielzug. Er kann jetzt wieder wählen, ob er 1 oder 2 Würfel verwendet, erhält evtl. Einkommen und darf 1 Unternehmen oder 1 Großprojekt bauen. Würfelt er in diesem zweiten Zug wieder einen Pasch, gilt das jedoch nicht erneut."));
+                new ProjectCard(FREIZEITPARK, UNTERNEHMEN, 16, "Nur wenn der Spieler seinen Bahnhof gebaut hat, darf er auch mit 2 Wuerfeln wuerfeln. Er kann dadurch 2 gleiche Zahlen erhalten (Pasch). Der Spieler hat in diesem Fall einen vollstaendigen zweiten Spielzug. Er kann jetzt wieder waehlen, ob er 1 oder 2 Wuerfel verwendet, erhaelt evtl. Einkommen und darf 1 Unternehmen oder 1 Grossprojekt bauen. Wuerfelt er in diesem zweiten Zug wieder einen Pasch, gilt das jedoch nicht erneut."));
         projectCards.put(FUNKTURM,
-                new ProjectCard(FUNKTURM, UNTERNEHMEN, 22, "Wenn der Spieler erneut würfelt, muss er die gleiche Anzahl Würfel verwenden, wie in seinem ersten Wurf. Es gilt dann nur das Ergebnis dieses zweiten Wurfs."));
+                new ProjectCard(FUNKTURM, UNTERNEHMEN, 22, "Wenn der Spieler erneut wuerfelt, muss er die gleiche Anzahl Wuerfel verwenden, wie in seinem ersten Wurf. Es gilt dann nur das Ergebnis dieses zweiten Wurfs."));
 
         businessCards.put(WEIZENFELD,
-                new BusinessCard(WEIZENFELD, LANDWIRTSCHAFT_PFLANZEN, 1, 1, "Erhalte 1 Münze aus der Bank."));
+                new BusinessCard(WEIZENFELD, LANDWIRTSCHAFT_PFLANZEN, 1, 1, "Erhalte 1 Muenze aus der Bank."));
         businessCards.put(BAUERNHOF,
-                new BusinessCard(BAUERNHOF, LANDWIRTSCHAFT_TIERE, 1, 2, "Erhalte 1 Münze aus der Bank."));
+                new BusinessCard(BAUERNHOF, LANDWIRTSCHAFT_TIERE, 1, 2, "Erhalte 1 Muenze aus der Bank."));
         businessCards.put(BAECKEREI,
-                new BusinessCard(BAECKEREI, INDUSTRIE_PRODUKTION, 1, 2, 3, "Erhalte 1 Münze aus der Bank."));
+                new BusinessCard(BAECKEREI, INDUSTRIE_PRODUKTION, 1, 2, 3, "Erhalte 1 Muenze aus der Bank."));
         businessCards.put(CAFE,
-                new BusinessCard(CAFE, RESTAURANTS, 2, 3, "Erhalte 1 Münze von dem Mitspieler, der eine '3' gewürfelt hat."));
+                new BusinessCard(CAFE, RESTAURANTS, 2, 3, "Erhalte 1 Muenze von dem Mitspieler, der eine '3' gewuerfelt hat."));
         businessCards.put(MINI_MARKT,
-                new BusinessCard(MINI_MARKT, INDUSTRIE_PRODUKTION, 2, 4, "Erhalte 3 Münzen aus der Bank."));
+                new BusinessCard(MINI_MARKT, INDUSTRIE_PRODUKTION, 2, 4, "Erhalte 3 Muenzen aus der Bank."));
         businessCards.put(WALD,
-                new BusinessCard(WALD, LANDWIRTSCHAFT_ROHSTOFFE, 3, 5, "Erhalte 1 Münze aus der Bank."));
+                new BusinessCard(WALD, LANDWIRTSCHAFT_ROHSTOFFE, 3, 5, "Erhalte 1 Muenze aus der Bank."));
         businessCards.put(STADION,
-                new BusinessCard(STADION, UNTERNEHMEN, 6, 6, "Erhalte von jedem Mitspieler 2 Münzen."));
+                new BusinessCard(STADION, UNTERNEHMEN, 6, 6, "Erhalte von jedem Mitspieler 2 Muenzen."));
         businessCards.put(FERNSEHSENDER,
-                new BusinessCard(FERNSEHSENDER, UNTERNEHMEN, 7, 6, "Erhalte von einem Mitspieler deiner Wahl 5 Münzen."));
+                new BusinessCard(FERNSEHSENDER, UNTERNEHMEN, 7, 6, "Erhalte von einem Mitspieler deiner Wahl 5 Muenzen."));
         businessCards.put(BUEROHAUS,
                 new BusinessCard(BUEROHAUS, UNTERNEHMEN, 8, 6, "Tausche 1 Karte mit einem Mitspieler deiner Wahl. Keine '" + UNTERNEHMEN + "' Karte."));
         businessCards.put(MOLKEREI,
-                new BusinessCard(MOLKEREI, INDUSTRIE_VERKAUF, 5, 7, "Erhalte 3 Münzen aus der Bank für jedes deiner '" + LANDWIRTSCHAFT_TIERE + "' Karten."));
+                new BusinessCard(MOLKEREI, INDUSTRIE_VERKAUF, 5, 7, "Erhalte 3 Muenzen aus der Bank fuer jedes deiner '" + LANDWIRTSCHAFT_TIERE + "' Karten."));
         businessCards.put(MOEBELFABRIK,
-                new BusinessCard(MOEBELFABRIK, INDUSTRIE_VERKAUF, 3, 8, "Erhalte 3 Münzen aus der Bank für jedes deiner '" + LANDWIRTSCHAFT_ROHSTOFFE + "' Karten."));
+                new BusinessCard(MOEBELFABRIK, INDUSTRIE_VERKAUF, 3, 8, "Erhalte 3 Muenzen aus der Bank fuer jedes deiner '" + LANDWIRTSCHAFT_ROHSTOFFE + "' Karten."));
         businessCards.put(BERGWERK,
-                new BusinessCard(BERGWERK, LANDWIRTSCHAFT_ROHSTOFFE, 6, 9, "Erhalte 5 Münzen aus der Bank."));
+                new BusinessCard(BERGWERK, LANDWIRTSCHAFT_ROHSTOFFE, 6, 9, "Erhalte 5 Muenzen aus der Bank."));
         businessCards.put(FAMILIENRESTAURANT,
-                new BusinessCard(FAMILIENRESTAURANT, RESTAURANTS, 3, 9, 10, "Erhalte 2 Münzen von dem Mitspieler, der eine '9' oder '10' gewürfelt hat."));
+                new BusinessCard(FAMILIENRESTAURANT, RESTAURANTS, 3, 9, 10, "Erhalte 2 Muenzen von dem Mitspieler, der eine '9' oder '10' gewuerfelt hat."));
         businessCards.put(APFELPLANTAGE,
-                new BusinessCard(APFELPLANTAGE, LANDWIRTSCHAFT_PFLANZEN, 3, 10, "Erhalte 3 Münzen aus der Bank."));
+                new BusinessCard(APFELPLANTAGE, LANDWIRTSCHAFT_PFLANZEN, 3, 10, "Erhalte 3 Muenzen aus der Bank."));
         businessCards.put(MARKTHALLE,
-                new BusinessCard(MARKTHALLE, INDUSTRIE_VERKAUF, 2, 11, 12, "Erhalte 3 Münzen aus der Bank für jedes deiner '" + LANDWIRTSCHAFT_PFLANZEN + "' Karten."));
+                new BusinessCard(MARKTHALLE, INDUSTRIE_VERKAUF, 2, 11, 12, "Erhalte 3 Muenzen aus der Bank fuer jedes deiner '" + LANDWIRTSCHAFT_PFLANZEN + "' Karten."));
     }
 
     public ProjectCard getProjectCard(ProjectCardEntity card) throws CardNotAvailableException {
