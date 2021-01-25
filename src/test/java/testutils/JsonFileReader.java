9103a1e5defca88a034900e72f21bb5055ae4246
Felix Steinke <82stfe1bif@hft-stuttgart.de>
2021-01-25 09:24:13 +0100

    update testutils with javadoc

--- src/test/java/testutils/JsonFileReader.java
+++ src/test/java/testutils/JsonFileReader.java
@@ -7,6 +7,13 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class JsonFileReader {
+    /**
+     * Reads a file from resources
+     *
+     * @param file path starting at src/test/resources
+     * @return content (json) as String
+     * @throws IOException file not found
+     */
     public static String readFile(String file) throws IOException {
         InputStream stream = JsonFileReader.class.getClassLoader().getResourceAsStream(file);
         assert stream != null;
